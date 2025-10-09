81d3fc072c9be7a3ee78bd27d27285219777c850
Cubicheng <2630377728@qq.com>
2025-10-09 09:21:30 +0800

    修复bug，优化体验

--- Assets/Scripts/CollectableBall.cs
+++ Assets/Scripts/CollectableBall.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class CollectableBall : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Light2D light2d;
+    private const float RespawnAnimationDuration = 0.6f;
     private void Start() {
         GameManager.Instance.RegisterBall(gameObject);
     }
@@ -17,6 +21,20 @@ public class CollectableBall : MonoBehaviour
 
     public void Respawn() {
         gameObject.SetActive(true);
-        animator.SetTrigger(AnimatorParams.CollectableBallRespawn);
+        StartCoroutine(RespawnAnimationRoutine());
+    }
+
+    IEnumerator RespawnAnimationRoutine() {
+        light2d.intensity = 0;
+        transform.localScale = Vector3.zero;
+        float time = 0;
+        while (time < RespawnAnimationDuration) {
+            time += Time.deltaTime;
+            light2d.intensity = Mathf.Lerp(0, 1, time / RespawnAnimationDuration);
+            transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, time / RespawnAnimationDuration);
+            yield return null;
+        }
+        light2d.intensity = 1;
+        transform.localScale = Vector3.one;
     }
 }
